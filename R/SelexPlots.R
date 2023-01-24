972a60191ca0bf930c39be387c6c08f658b9e17d
michaelgras <michael.gras@ec.europa.eu>
2023-01-24 14:39:38 +0100

    ploteqselex_2

--- R/SelexPlots.R
+++ R/SelexPlots.R
@@ -533,3 +533,171 @@ mleg = function(size=0.5,height=0.5,width=0.5,title=6,text=6){
 }  
 #}}}
 
+
+
+#{{{
+#' ploteqselex()
+#
+#' Isopleth plot of Selectivity vs F
+#'
+#' @param brps output from brp.selex() 
+#' @param fit output from () 
+#' @param Fmax upper possible limit of  F range
+#' @param panels choice of plots 1:4
+#' \itemize{
+#'   \item 1  F over Relative Yield
+#'   \item 2  F over SRP or SSB (requires ssr)
+#'   \item 3  Isopleth Yield
+#'   \item 4  Isopleth SPR or SSB (requires ssr)
+#' }    
+#' @param ncol number of columns
+#' @param colours optional, e.g. terrain.col, rainbow, etc.
+#' @param Ftrg  option to dynamic Ftrg=c("none","fmsy","f0.1")
+#' \itemize{
+#'   \item "none"  
+#'   \item "msy"  
+#'   \item "f0.1"
+#' }
+#' @return ggplot   
+#' @export
+ploteqselex_2 = function(brps,fit,Fmax=2.,panels=NULL, ncol=NULL,colours=NULL,Ftrg=c("none","msy","f0.1")){
+  # Colour function
+  if(is.null(colours)){colf = r4col} else {colf = colours}
+  if(is.null(panels)) panels=1:4
+  if(is.null(ncol)){
+    if(length(panels)%in%c(1,3)){ncol=length(panels)} else {ncol=2}
+  }
+  # instead of observed we choose the brp that corresponds to fitted s50
+  obsS <- names(pars)[an(names(pars)) >= round(an(fit$par[1]),1)][1]
+  
+  # Check range 
+  # WARNING: Also include na.rm = TRUE in order to get the limits (lim) in case the fmle did not converged all the times
+  if(paste(brps[[1]]@model)[3]%in%c("a + ssb/ssb - 1")){
+    pr = TRUE
+    lim = min(Fmax,max(2*refpts(brps[[1]])["f0.1","harvest"],refpts(brps[[1]])["Fref","harvest"]*1.5,dims(brps[[1]])[["min"]]), na.rm = TRUE)
+    quants = c("YPR","SPR")
+    labs = c("Relative YPR",expression(SPR/SPR[0]))
+    
+  } else {
+    pr = FALSE
+    lim = min(Fmax,max(2*refpts(brps[[3]])["msy","harvest"],refpts(brps[[1]])["Fref","harvest"]*1.05,dims(brps[[1]])[["min"]]), na.rm = TRUE)
+    quants = c("Yield","SSB")
+    labs = c("Relative Yield",expression(SBB/SSB[0]))
+  }
+  
+  # Prep some data for plotting
+  fbar(brps[[1]]) = seq(0,lim,lim/101)[1:101]
+  obs = data.frame(obs="obs",model.frame(metrics(brps[[1]],list(ssb=ssb, harvest=fbar, rec=rec, yield=landings)),drop=FALSE))
+  obs[,8:11][obs[,8:11]<0] <- 0
+  S50 = as.list(an(brps[-1]@names))
+  isodat = do.call(rbind,Map(function(x,y){
+    fbar(x) = seq(0,lim,lim/101)[1:101]
+    mf =  model.frame(metrics(x,list(ssb=ssb, harvest=fbar, rec=rec, yield=landings)),drop=FALSE)
+    data.frame(S50=y,as.data.frame(mf))  
+  },brps[-1],S50))
+  isodat$yield = isodat$yield#/max(isodat$yield)
+  isodat[,8:11][isodat[,8:11]<0] <- 0
+  Fobs = an(refpts(brps[[obsS]])["Fref","harvest"])
+  Yobs = an(refpts(brps[[obsS]])["Fref","yield"])
+  Sobs = an(refpts(brps[[obsS]])["Fref","ssb"])#/an(refpts(brps[[1]])["virgin","ssb"])
+  # Sa=brps[[1]]@landings.sel/max(brps[[1]]@landings.sel)
+  # S50obs = s50(Sa)
+  S50obs = an(fit$par[1])
+  isodat$Fo = c(obs$harvest,rep(NA,nrow(isodat)-nrow(obs)))
+  isodat$Yo = c(obs$yield,rep(NA,nrow(isodat)-nrow(obs)))/max(isodat$yield)
+  isodat$So = c(obs$ssb,rep(NA,nrow(isodat)-nrow(obs)))/max(isodat$ssb)
+  
+  if(Ftrg[1]!="none"){
+    ftrg = do.call(rbind,Map(function(x,y){
+      frp= c(refpts(x)[Ftrg,"harvest"])
+      data.frame(s50=y,ftrg=frp)
+    },brps[-1],S50))
+    ftrg = ftrg[ftrg$ftrg<lim,]
+  }
+  # F vs Yield
+  P1 = ggplot(data=isodat,aes(x=harvest,y=yield/max(yield),group=S50))+
+    geom_line(aes(color=S50))+geom_line(aes(x=Fo,y=Yo),size=0.7,linetype="dashed", na.rm=TRUE)+
+    scale_color_gradientn(colours=rev(colf(20)))+ylab(labs[1])+
+    geom_segment(aes(x = Fobs, xend = Fobs, y = 0, yend = Yobs/max(yield)), colour = "black",size=0.3,linetype="dotted")+
+    geom_segment(aes(x = 0, xend = Fobs, y = Yobs/max(yield), yend = Yobs/max(yield)), colour = "black",size=0.3,linetype="dotted")+
+    geom_point(aes(x=Fobs,y=Yobs/max(yield)),size=2)+
+    xlab("Fishing Mortality")+
+    theme(legend.key.size = unit(1, 'cm'), #change legend key size
+          legend.key.height = unit(1, 'cm'),
+          legend.text = element_text(size=7),
+          legend.key.width = unit(0.6, 'cm'),
+          axis.title=element_text(size=10),
+          legend.title=element_text(size=9)
+    )+
+    scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0),limits=c(0,1))
+  # F vs SSB  
+  P2 = ggplot(data=isodat,aes(x=harvest,y=ssb/max(ssb),group=S50))+
+    geom_line(aes(color=S50))+geom_line(aes(x=Fo,y=So),size=0.7,linetype="dashed", na.rm=TRUE)+
+    scale_color_gradientn(colours=rev(colf(20)))+
+    geom_segment(aes(x = Fobs, xend = Fobs, y = 0, yend = Sobs/max(ssb)), colour = "black",size=0.3,linetype="dotted")+
+    geom_segment(aes(x = 0, xend = Fobs, y = Sobs/max(ssb), yend = Sobs/max(ssb)), colour = "black",size=0.3,linetype="dotted")+
+    geom_point(aes(x=Fobs,y=Sobs/max(ssb)),size=2)+
+    ylab(labs[2])+xlab("Fishing Mortality")+
+    theme(legend.key.size = unit(1, 'cm'), #change legend key size
+          legend.key.height = unit(1, 'cm'),
+          legend.key.width = unit(0.6, 'cm'),
+          legend.text = element_text(size=7),
+          axis.title=element_text(size=10),
+          legend.title=element_text(size=9)
+    )+ 
+    scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0),limits=c(0,1))
+  
+  # Isopleth plot Yield
+  colbr = c(seq(0,0.6,0.2),seq(0.7,0.9,0.1),0.95,1)
+  conbr = c(0,0.2,0.4,seq(0.5,0.9,0.1),0.95,1)
+  nbr = length(colbr)
+  P3=ggplot(isodat, aes(x=harvest,y=S50))+
+    geom_raster(aes(fill = yield/max(yield)), 
+                interpolate = T, hjust = 0.5, vjust = 0.5)+ 
+    metR::geom_contour2(aes(z=yield/max(yield)),color = grey(0.4,1),breaks =conbr )+ 
+    metR::geom_text_contour(aes(z=yield/max(yield)),stroke = 0.2,size=3,skip=0,breaks = conbr)+
+    scale_fill_gradientn(colours=rev(colf(nbr+3))[-c(10:11,13)],limits=c(-0.03,1), breaks=colbr, name=paste(quants[1]))+
+    geom_point(aes(x=Fobs,y=S50obs),size=2.5)+
+    geom_segment(aes(x = Fobs, xend = Fobs, y = min(S50), yend = S50obs), colour = "black",size=0.3,linetype="dotted")+
+    geom_segment(aes(x = 0, xend = Fobs, y = S50obs, yend = S50obs), colour = "black",size=0.3,linetype="dotted")+
+    theme(legend.key.size = unit(1, 'cm'), #change legend key size
+          legend.key.height = unit(1, 'cm'),
+          legend.key.width = unit(0.6, 'cm'),
+          legend.text = element_text(size=7),
+          axis.title=element_text(size=10),
+          legend.title=element_text(size=9)
+    )+
+    scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(-0.03, 0))+
+    ylab("Age-at-50%-Selectivity")+xlab("Fishing Mortality")
+  
+  # Isopleth SSB
+  colbr = c(0.05,seq(0,1,0.1))
+  conbr = c(seq(0.05,0.4,0.05),seq(0.5,0.6,1),1)
+  nbr = length(colbr)
+  P4 = ggplot(isodat, aes(x=harvest,y=S50))+
+    geom_raster(aes(fill = ssb/max(ssb)), 
+                interpolate = T, hjust = 0.5, vjust = 0.5)+ 
+    metR::geom_contour2(aes(z=ssb/max(ssb)),color = grey(0.4,1),breaks =conbr )+
+    metR::geom_text_contour(aes(z=ssb/max(ssb)),stroke = 0.2,size=3,skip=0,breaks = conbr)+
+    scale_fill_gradientn(colours=rev(colf(nbr+4))[-c(4:7)],limits=c(-0.03,1), breaks=colbr, name=quants[2])+
+    theme(legend.key.size = unit(1, 'cm'), #change legend key size
+          legend.key.height = unit(1, 'cm'),
+          legend.key.width = unit(0.6, 'cm'),
+          legend.text = element_text(size=7),
+          axis.title=element_text(size=10),
+          legend.title=element_text(size=9)
+    )+
+    geom_point(aes(x=Fobs,y=S50obs),size=2.5)+
+    geom_segment(aes(x = Fobs, xend = Fobs, y = min(S50), yend = S50obs), colour = "black",size=0.3,linetype="dotted")+
+    geom_segment(aes(x = 0, xend = Fobs, y = S50obs, yend = S50obs), colour = "black",size=0.3,linetype="dotted")+
+    scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(-0.03, 0))+
+    ylab("Age-at-50%-Selectivity")+xlab("Fishing Mortality")
+  if(Ftrg[1]!="none"){
+    P3 = P3+geom_point(data=ftrg,aes(x=ftrg,y=s50),shape = 21, colour = "black",size=1.1, fill = "white")
+    P4= P4+geom_point(data=ftrg,aes(x=ftrg,y=s50),shape = 21, colour = "black",size=1.1, fill = "white")
+  }
+  plots <- list(P1=P1,P2=P2,P3=P3,P4=P4)
+  
+  if(length(panels)>1) return(gridExtra::grid.arrange(grobs =  plots[panels], ncol = ncol))  
+  if(length(panels)==1) return(plots[[panels]])  
+}
